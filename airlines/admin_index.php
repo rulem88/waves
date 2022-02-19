dbf5bc9d4d6491ad54a861c628981ae080b237d4
Владислав Дроздов <90255521+prplV@users.noreply.github.com>
2022-02-19 14:57:39 +0300

    updated files with fixed bugs

---
+++ airlines/admin_index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ADMIN_Waves</title>
+        <link rel="stylesheet" href="style.css">
+        <script async src="scripts/jquery.js"></script>
+        
+        <script>
+
+        </script>
+
+        <style>
+            
+            
+        </style>
+    </head>
+    <body>
+        <?php include("blocks/header_admin.php"); ?>
+
+
+    </body>
+
+</html>
---
+++ airlines/blocks/gallery.php
@@ -0,0 +1,18 @@
+<?php
+    echo "<div class='content-verst'>
+    <div class='content-pic-pos-1st-line'>
+        <img src='pictures/m_1.jpg' width='400px' height='200px'>
+        <img src='pictures/2_m.png' width='400px' height='200px'>
+        <img src='pictures/m_3.jpg' width='400px' height='200px'>
+    </div>
+    <br>
+    <div class='content-pic-pos-2st-line'>
+        <span class='gal-4'></span>
+        <img src='pictures/m_4.jpg' width='400px' height='200px'>
+        <span class='gal-5'></span>
+        <img src='pictures/m-5.jpg' width='400px' height='200px'>
+        <span class='gal-6'></span>
+        <img src='pictures/m_6.jpg' width='400px' height='200px'>
+    </div>
+</div>";
+?>
