09b259d1ff484cb6fd1c2245708f03d3094dbc9d
Владислав Дроздов <90255521+prplV@users.noreply.github.com>
2022-02-20 13:41:45 +0300

    it s okey to cry
    it s okey to cry

--- airlines/blocks/header_admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo "<header>
-    <div class='shapka-settings'>
-        <img src='pictures/m_icon.svg' height='50px' width='55px'>
-        <span class='span-header-style'>Waves_admin</span>
-    </div>
-</header>";
-?>
